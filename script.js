563ae7105ddc102ff331405080940917559e4d1b
Solomon <102990501+sollhaile@users.noreply.github.com>
2023-12-24 00:47:06 +0300

    Some How

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icons = document.querySelector(".nav-two-icons");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
